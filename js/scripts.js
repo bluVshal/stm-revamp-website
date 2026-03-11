5d5663abd8d7e7878ee69fbfa2accb80142960a2
PraVishal <blu.vishal@gmail.com>
2026-03-11 16:50:33 +0400

    Added more modifications

--- js/scripts.js
+++ js/scripts.js
@@ -221,3 +221,45 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+
+/**************************************************** */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+  const waveHeight = window.innerHeight / 16;
+
+  sections.forEach((section, index) => {
+    const width = section.offsetWidth;
+    const height = section.offsetHeight;
+    const nextSection = section.nextElementSibling;
+
+    section.style.zIndex = sections.length - index;
+
+    if (nextSection?.tagName === "SECTION") {
+      nextSection.style.marginTop = `-${waveHeight}px`;
+    }
+
+    if (!section.classList.contains("wave-divider-bottom")) return;
+
+    let wavePathData = "M0 " + (height - waveHeight / 2) + " ";
+    for (let x = 0; x <= width; x++) {
+      const y =
+        waveHeight / 2 +
+        (waveHeight / 2) *
+          Math.sin((x / width) * ((width * 2) / height) * Math.PI);
+      wavePathData += `L${x} ${height - y} `;
+    }
+    wavePathData += `L${width} 0 L0 0 Z`;
+
+    section.style.clipPath = `path('${wavePathData}')`;
+  });
+});
+
+window.addEventListener("resize", function () {
+  this.document.dispatchEvent(new Event("DOMContentLoaded"));
+});
+
+
+/******************************************************** */
